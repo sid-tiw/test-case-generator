3251db9d465296bcb0094e13606d21b7b7e5f595
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2019-11-06 11:26:07 +0530

    trying to add next page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -52,6 +53,24 @@ namespace test_case_generator
             {
                 // Application now has read/write access to the picked file
                 textBox.Text = file.Path;
+                /*StreamReader sr = new StreamReader(file.Path);*/
+                FileStream ff = new FileStream(file.Path, FileMode.Open, FileAccess.Read);
+                /*string content = await sr.ReadToEndAsync();*/
+                long n = ff.Length;
+                byte []arr = new byte[n];
+                string content = "";
+                long r = 0, rt = n;
+                while(rt > n)
+                {
+                    int res = ff.Read(arr, (int)r, (int)rt);
+                    if (res == 0)
+                        break;
+                    r += res;
+                    rt -= res;
+                }
+                for (int i = 0; i < r; i++)
+                    content += (char)arr[i];
+                Code.Text = content;
             }
             else
             {
@@ -59,24 +78,26 @@ namespace test_case_generator
             }
         }
 
-        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        /*private void textBox_GotFocus(object sender, RoutedEventArgs e)
         {
             textBox.SelectAll();
         }
 
-        private void previous_Click(object sender, RoutedEventArgs e)
+        *//*private void previous_Click(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
-        }
+        }*/
 
         private void next_Click(object sender, RoutedEventArgs e)
         {
-            ControlTemplate t = next.Template;
-            Button bn = new Button();
-            bn.Template = t;
-            var str = bn.ActualTheme.ToString();
-            Console.WriteLine(str);
-            /*path = textBox.Text;
+            var str1 = canva1.ActualHeight.ToString();
+            var str2 = canva1.ActualWidth.ToString();
+            textBox.Text = str1 + " " + str2;
+            Frame.Navigate(typeof(newPage));
+            //Frame.Navigate(typeof(newPage));
+            /*global::System.Uri resourceLocator = new global::System.Uri("ms-appx:///newPage.xaml");
+            global::Windows.UI.Xaml.Application.LoadComponent(this, resourceLocator, global::Windows.UI.Xaml.Controls.Primitives.ComponentResourceLocation.Application);
+            *//*path = textBox.Text;
             string str = "";
             var n = path.Length;
             for (int i = n - 4; i < n; i++)
@@ -85,9 +106,26 @@ namespace test_case_generator
                 textBox.Text = "You didn't entered a valid address";*/
         }
 
-        private void Finish_Click(object sender, RoutedEventArgs e)
+        /*private void Finish_Click(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
+        }*/
+
+        private void Code_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Tab)
+                Code.Text += "    ";
         }
+
+        /*private void browse_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            *//*# FF39FD66*//*
+            browse.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 57, 253, 102));
+        }
+
+        private void browse_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            *//*browse.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 69, 138, 255));*//*
+        }*/
     }
 }
